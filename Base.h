68e5e65c1abc49928a52193822bf45032d83d7c9
aravbatra <arav.batra@gmail.com>
2017-02-14 02:39:01 -0800

    Created Base.h

---
+++ Base.h
@@ -0,0 +1,14 @@
+#ifndef __BASE_H__
+#define __BASE_H__
+
+using namespace std;
+
+class Base{
+public:
+  //virtual exec that executes leaf/compostite node in the tree
+  virtual bool exec() = 0;
+
+}
+
+
+#endif
