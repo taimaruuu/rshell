3532926402543fbb98379c504df5a88e001897e2
aravbatra <arav.batra@gmail.com>
2017-02-14 03:06:00 -0800

    Changed return type for exec() to void from bool

--- Base.h
+++ Base.h
@@ -4,11 +4,11 @@
 using namespace std;
 
 class Base{
-public:
-  //virtual exec that executes leaf/compostite node in the tree
-  virtual bool exec(string cmd = " ") = 0;
+  public:
+    //virtual exec that executes leaf/compostite node in the tree
+    virtual void exec(string cmd = " ") = 0;
 
-}
+};
 
 
 #endif
