729bff0dfa710c757a9fa60747ee6f4844cc48ae
aravbatra <arav.batra@gmail.com>
2017-02-16 17:22:23 -0800

    added main.cpp with prompt

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <iostream>
+
+using namespace std;
+int main(int argc, char**argv) {
+  //loop for the shell
+  //only exits with exit command
+  for (; ; ) {
+
+    char *userName = getlogin();            //Grab the user's name
+    if(!userName) {
+        perror("getlogin() error");         //Just in case you can't find it
+    }
+
+    char hostName[1000];
+    gethostname(hostName, sizeof hostName); //Grab the machine name
+
+    cout << userName << "@" <<  hostName << "$ ";                           //Prints the bash $
+    //End of login info
+    return 0;
+  }
+
+}
