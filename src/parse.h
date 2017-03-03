4cddeef96d418280eafbb346bf24b55a1d73680a
aravbatra <arav.batra@gmail.com>
2017-03-02 22:03:01 -0800

    fixed quote error

--- src/parse.h
+++ src/parse.h
@@ -39,11 +39,13 @@ Base* tokeParse(tokenizer &token, tokenizer::iterator &it){
         ret = new Or(new Command(arglist),tokeParse(token,it));
       }
       if(cast.at(0) == ";"){
-
         commandOnly = false;
         return new Semicolon(new Command(arglist),tokeParse(token,it));
       }
+      // if(cast.at(0) == "[")
+
       arglist.push_back(cast.at(0));
+      
       if (it != token.end()) {
         it++;
       }
@@ -60,12 +62,18 @@ Base* tokeParse(tokenizer &token, tokenizer::iterator &it){
 
 Base* parse(string &input){
   typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
+  // boost::char_separator<char> sep("\"");
   vector<string> arglist;
   vector<string> cast(1);
   Base *ret = 0;
   bool commandOnly = true;
   tokenizer token(input);
   tokenizer::iterator it = token.begin();
+  // cout << "Tokenizer" << endl;
+  // for(tokenizer::iterator beg = token.begin(); beg!=token.end();++beg){
+  //     cout << *beg << "\n";
+  // }
+
     int i = 0;
     while (it != token.end()) {
       if(it != token.end()){
@@ -98,6 +106,15 @@ Base* parse(string &input){
     }
     return ret;
 }
+
+void preParse(string &input){
+  for(unsigned i = 0; i < input.size(); i++){
+    if(input[i] == '\"' || input[i] == '\'' ){
+      input.erase(input.begin()+i);
+    }
+  }
+}
+
 
 
 
---
+++ test.h
@@ -0,0 +1,16 @@
+#ifndef __TEST_H__
+#define __TEST_H__
+
+#inclue "Base.h"
+using namespace std;
+
+class Test: public Base {
+protected: 
+
+
+};
+
+
+
+
+#endif
