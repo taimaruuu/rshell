03435c426591d87f45111f77984ca017749ff641
Trevor <tkim057@ucr.edu>
2017-03-03 18:35:43 -0800

    changed test a bit

--- tests/test_test.sh
+++ tests/test_test.sh
@@ -15,26 +15,26 @@ echo "Expected outcome: (True), (True), (True), (False)"
 [-f text]
 [-d text]
 
-echo "Making directory named test_dir"
-mkdir test_dir
+echo "Making directory named sample_dir"
+mkdir sample_dir
 echo "Testing no flag, -e, -f, d, on directory"
 echo "Expected outcome: (False), (False), (False), (True)"
-test test_dir
-test -e test_dir
-test -f test_dir
-test -d test_dir
+test sample_dir
+test -e sample_dir
+test -f sample_dir
+test -d sample_dir
 echo "Testing no flag, -e, -f, d, on directory with brackets"
 echo "Expected outcome: (False), (False), (False), (True)"
-[test_dir]
-[-e test_dir]
-[-f test_dir]
-[-d test_dir]
-cd test_dir
+[sample_dir]
+[-e sample_dir]
+[-f sample_dir]
+[-d sample_dir]
+cd sample_dir
 echo "Making file to test longer paths"
 touch text1
 echo "Testing using brackets and -f flag"
 cd ../
-[-f test_dir/text1]
+[-f sample_dir/text1]
 echo "Test invalid flag and then invalid path/file"
 test -p text
 [this/is/an/invaild/path]
